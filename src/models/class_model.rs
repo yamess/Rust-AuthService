98d5a8255892b7965ad30b8b6efab4e11211105e
Boukari Yameogo <yameogo.boukari@gmail.com>
2024-03-02 01:15:17 -0500

    fix

--- src/models/class_model.rs
+++ src/models/class_model.rs
@@ -3,6 +3,8 @@ use diesel::{AsChangeset, Identifiable, Insertable, Queryable, Selectable};
 use serde::{Deserialize, Serialize};
 use uuid::Uuid;
 
+use crate::schema::classes;
+
 #[derive(
 Insertable,
 Queryable,
@@ -14,7 +16,7 @@ AsChangeset,
 Debug,
 PartialEq,
 )]
-#[diesel(table_name = schedules)]
+#[diesel(table_name = classes)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct ClassModel {
     pub id: Uuid,
